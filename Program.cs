3c2c875178b05edbe1d76cafbeed6afb18ca51bd
JoseVictorLima <jsvctr123@gmail.com>
2019-05-20 09:56:58 -0300

    Commit Versão 1.2

--- Program.cs
+++ Program.cs
@@ -18,12 +18,90 @@ namespace IA
             //x = Sujo
             //O = Agente
             //roomStatus serve para auxiliar na hora de checar se esta tudo limpo onde true é sujo e false é limpo
-            rooms[0,2] = 'x';
-            roomStatus[0,2] = true;
-            rooms[1,0] = 'x';
-            roomStatus[1,0] = true;
-            rooms[2,1] = 'x';
-            roomStatus[2,1] = true;
+            Console.WriteLine("Escolha 3 salas sujas de 2 a 9");
+            for(int i = 0;i<3;i++){
+                int room = Int32.Parse(Console.ReadLine());
+                switch(room){
+                    // case 1 : 
+                    //     if(rooms[0,0]=='x'){
+                    //         Console.WriteLine("A sala já foi escolhida");
+                    //         i--;
+                    //     }
+                    //     else rooms[0,0] = 'x';
+                    //     break;
+                    case 2 : 
+                        if(rooms[0,1]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[0,1] = 'x';
+                        break;
+                    case 3 : 
+                        if(rooms[0,2]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[0,2] = 'x';
+                        break;
+                    case 4 : 
+                        if(rooms[1,0]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[1,0] = 'x';
+                        break;
+                    case 5 : 
+                        if(rooms[1,1]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[1,1] = 'x';
+                        break;
+                    case 6 : 
+                        if(rooms[1,2]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[1,2] = 'x';
+                        break;
+                    case 7 : 
+                        if(rooms[2,0]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[2,0] = 'x';
+                        break;
+                    case 8 : 
+                        if(rooms[2,1]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[2,1] = 'x';
+                        break;
+                    case 9 : 
+                        if(rooms[2,2]=='x'){
+                            Console.WriteLine("A sala já foi escolhida");
+                            i--;
+                        }
+                        else rooms[2,2] = 'x';
+                        break;
+                    default : 
+                        Console.WriteLine("Sala Invalida");
+                        i--;
+                        break;
+                }
+            }
+            for(int i =0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(rooms[i,j]=='x')roomStatus[i,j] = true;
+                }
+            }
+            // rooms[0,2] = 'x';
+            // roomStatus[0,2] = true;
+            // rooms[1,0] = 'x';
+            // roomStatus[1,0] = true;
+            // rooms[2,1] = 'x';
+            // roomStatus[2,1] = true;
             Robot agente = new Robot(0,0);
             bool allClear = false; //False = Tudo sujo; True = Tudo Limpo
             rooms[agente.MyY,agente.MyX] = 'O';
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 <h2>Agente inteligente apartir de um <i>Vacuum Cleaner</i>.</h2>
 <h3>Versão 1.0:</h3>
 <ul>
-	<li>Apenas percorre as salas de forma aleatória e para quando todas as salas estão limpas</li>
+	<li>Apenas percorre as salas de forma aleatória e para quando todas as salas estão limpas.</li>
 </ul>
 <h3>Versão 1.1:</h3>
 <ul>
 	<li>O Agente agora possui uma memória que guarda o caminho tomado apos limpar uma sala e caso venha a visitar essa sala novamente ele não utiliza mais o mesmo caminho utilizado antes.</li>
+</ul>
+<h3>Versão 1.2:</h3>
+<ul>
+	<li>Adição da possibilidade de escolher as salas que estarão sujas.</li>
 </ul>
